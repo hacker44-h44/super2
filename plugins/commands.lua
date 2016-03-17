00b5840abc60b53115106ac54e50f2994ab67c54
hacker44 <hacker.ht44@gmail.com>
2016-03-17 19:47:15 +0330

    Update commands.lua

--- plugins/commands.lua
+++ plugins/commands.lua
@@ -58,8 +58,8 @@ do
 
     return {
         patterns = {
-            "^#(help)$",
-            "^#(help) (.+)"
+            "^!(help)$",
+            "^!(help) (.+)"
         }, 
         run = run 
     }
