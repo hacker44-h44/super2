36dd5d7963264db044901ad5e0dc9c38115685b7
hacker44 <hacker.ht44@gmail.com>
2016-03-17 23:07:36 +0330

    Added files via upload

--- plugins/del.lua
+++ plugins/del.lua
@@ -1,36 +1,86 @@
 do
---create by RoyalTeam ID CHANNEL : @RoyalTeamCh >>>>>>tnx to @@Mehrpouya
-local function get_message_callback(extra, success, result)
-  if result.to.peer_type == 'channel' then
-    if our_id == result.from.peer_id then
-      --local del = delete_msg(result.id, ok_cb, true)
-    else
-      local del = delete_msg(result.id, ok_cb, true)
-      if del == false then
-        send_msg(extra, "deleting failed.", ok_cb, false)
-        return
-      end
-    end
-  else
-    send_msg(extra, "", ok_cb, false)
-    return
+local function pre_process(msg)
+  if not msg.text and msg.media then
+    msg.text = '['..msg.media.type..']'
   end
+  return msg
+end
+local function del_by_reply(extra, success, result)
+  vardump(result)
+    if result.to.peer_type == "channel" then
+        delete_msg(result.id, ok_cb, false)
+    end
+end
+function status_msg(msg, trigger)
+  local data = load_data(_config.moderation.data)
+  local strict_settings = data[tostring(msg.to.id)]['settings']['strict']
+  local status = trigger.." is not allowed here\n"..('@'..msg.from.username or msg.from.first_name).."\nStatus:"
+  if strict_settings == "yes" then
+    channel_kick_user("channel#id"..msg.to.id, "user#id"..msg.from.id, ok_cb, false)
+	delete_msg(msg.id, ok_cb, false)
+	status = status.."User kicked/msg deleted"
+  else
+    delete_msg(msg.id, ok_cb, false)
+	status = status.."Mssage deleted"
+  end
+  send_large_msg(get_receiver(msg), status)
 end
 local function run(msg, matches)
-  if matches[1] == "del" and msg.reply_id then
-    msgr = get_message(msg.reply_id,get_message_callback, get_receiver(msg))
+  local data = load_data(_config.moderation.data)
+  if data[tostring(msg.to.id)] then
+    if data[tostring(msg.to.id)]['settings']['lock_link'] == "yes" then
+      if msg.text:match("telegram.me") and not is_momod(msg) then
+		  delete_msg(msg.id, ok_cb, true)
+      end
+      if msg.media then
+        if msg.media.caption:match("telegram.me") and not is_momod(msg) then
+          delete_msg(msg.id, ok_cb, true)
+        end
+      end
+    end
   end
+  if data[tostring(msg.to.id)] then
+    if data[tostring(msg.to.id)]['settings']['lock_sticker'] == "yes" then
+	  if msg.media then
+	    if msg.media.caption == "sticker.webp" and not is_momod(msg) then
+	      delete_msg(msg.id,ok_cb, true)
+	    end
+	  end
+	end
+	if data[tostring(msg.to.id)]['settings']['lock_arabic'] == "yes" then
+	  if msg.text:match('[\216-\219][\128-\191]') and not is_momod(msg) then
+	    local trigger = "Persian"
+	    status_msg(msg, trigger)
+	  end
+	end
+  end
+  if data[tostring(msg.to.id)] then
+    if data[tostring(msg.to.id)]['settings']['lock_rtl'] == "yes" then
+	  if msg.text:match('‮') or msg.text:match('‏') and not is_momod(msg) then
+	    local trigger = "RTL characters"
+	    status_msg(msg, trigger)
+	  end
+	end
+  end
+  if matches[1] == "del" and is_momod(msg) then
+    if type(msg.reply_id)~= "nil" then
+      msgreply = get_message(msg.reply_id, del_by_reply, false)
+	end
+  end
+  return 
 end
---create by RoyalTeam ID CHANNEL : @RoyalTeamCh >>>>tnx to @Mehrpouya
 return {
-   description = "del-msg by reply.",
-   usage = {
-   },
-   patterns = {
-      "^[!/#](del)$"
-   },
-   run = run,
-   moderated = true,
-   hide = true
-
+  patterns = {
+    "(telegram.me)",
+	'‮', -- rtl
+	'‏', -- other rtl
+	"%[(photo)%]",
+	"([\216-\219][\128-\191])",
+	"^[/!#](del)$",
+	'%[(document)%]',
+	"^!!tgservice (.+)$"
+  },
+  run = run,
+  pre_process = pre_process
 }
+end
